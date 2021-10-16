e446be4145e62017fbd671b94ecd1629daa537f3
cf-19 <44059402+cf-19@users.noreply.github.com>
2021-10-16 12:16:36 +0800

    1

--- read-and-debug/index.js
+++ read-and-debug/index.js
@@ -2,40 +2,64 @@ import * as THREE from '../src/Three.js';
 
 class App {
 
-    camera; scene; renderer;
-    geometry; material; mesh;
+    camera; 
+    scene; 
+    renderer;
+    geometry; 
+    material; 
+    mesh;
 
     static VIEWPORT_WIDTH = 600;
     static VIEWPORT_HEIGHT = 400;
 
     init() {
-        this.camera = new THREE.PerspectiveCamera( 70, App.VIEWPORT_WIDTH / App.VIEWPORT_HEIGHT, 0.01, 10 );
-        this.camera.position.z = 1;
-    
+
+        this.camera = new THREE.PerspectiveCamera( 70, App.VIEWPORT_WIDTH / App.VIEWPORT_HEIGHT, 0.01, 1000 );
+        this.camera.position.z = 100;
         this.scene = new THREE.Scene();
     
-        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-        this.material = new THREE.MeshNormalMaterial();
-    
-        this.mesh = new THREE.Mesh( this.geometry, this.material );
-        this.scene.add( this.mesh );
+        /** custom start */
+        this.testShapeGeometry()
+        /** custom end */
     
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
         this.renderer.setSize( App.VIEWPORT_WIDTH, App.VIEWPORT_HEIGHT );
         this.renderer.setAnimationLoop( this.animation.bind(this) );
         document.body.appendChild( this.renderer.domElement );
+
     }
 
-    /**
-     * 
-     * @param {Number} time each frame take time?
+     /**
+     * @param {Number} 0 - frames
      */
     animation(time) {
         // console.log(time);
-        this.mesh.rotation.x = time / 2000;
-        this.mesh.rotation.y = time / 1000;
+        // this.mesh.rotation.x = time / 2000;
+        // this.mesh.rotation.y = time / 1000;
     
         this.renderer.render( this.scene, this.camera );
+    }
+
+    /** test shape geometry */
+    testShapeGeometry() {
+
+        const x = 0, y = 0;
+
+        const heartShape = new THREE.Shape();
+
+        heartShape.moveTo( x + 5, y + 5 );
+        heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
+        heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
+        heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
+        heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
+        heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
+        heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+
+        const geometry = new THREE.ShapeGeometry( heartShape );
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        const mesh = new THREE.Mesh( geometry, material ) ;
+        this.scene.add( mesh );
+
     }
 
 }
